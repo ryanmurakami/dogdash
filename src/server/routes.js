9bfe626344eb49447d0d402fd6eeed3163c83a51
ryanmurakami <ryanharrisonlewis@gmail.com>
2020-07-17 11:41:20 -0600

    guh

--- src/server/routes.js
+++ src/server/routes.js
@@ -12,7 +12,7 @@ async function indexHandler (req, res) {
   const dynamoStatus = await db.test()
   const dynaMessage = dynamoStatus.connected
     ? 'connected'
-    : `not connected. ${dynamoStatus.error} ${JSON.stringify(process.env)} ${dynamoStatus.res}`
+    : `not connected. ${dynamoStatus.error} ${JSON.stringify(dynamoStatus.res)}`
   res.send(`Hello from DogDash. Dynamo is ${dynaMessage}.`)
 }
 
